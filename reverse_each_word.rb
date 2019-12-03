385aa6fdd45804b0d7793f872e1c5c43ee9e09de
T Benson <t.benson@live.co.uk>
2019-12-03 11:13:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 def reverse_each_word (string)
   string.split.collect { |word| word.reverse! }.join(" ")
-  # string.join(" ")
+
 end
 
-# def reverse_each_word_collect (string)
-#   new_string = string.split
-#   new_string.collect { |word| word.reverse! }
-#   new_string.join(" ")
-#
-# end
