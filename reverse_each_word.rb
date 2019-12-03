f36261465a197221b0633ae5ef3774cabede485f
T Benson <t.benson@live.co.uk>
2019-12-03 10:56:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word (string)
-  new_string = %w(string).each do |word|
-    word.reverse end
-  new_string.join
+  new_string = %w(string)
+  # each do |word|
+  #   word.reverse end
+  # new_string.join
 
 end
